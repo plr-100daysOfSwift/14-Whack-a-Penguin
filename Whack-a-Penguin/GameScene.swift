68a528418a6182bfbd990551c96521f36fdc2338
Paul Richardson <developer@photographerafoot.com>
2021-05-09 12:24:04 +1000

    CHG: function createSlot()

--- Whack-a-Penguin/GameScene.swift
+++ Whack-a-Penguin/GameScene.swift
@@ -10,6 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+	var slots = [WhackSlot]()
 	var gameScore: SKLabelNode!
 	var score = 0 {
 		didSet {
@@ -33,6 +34,13 @@ class GameScene: SKScene {
 	}
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+	}
+
+	func createSlot(at position: CGPoint) {
+		let slot = WhackSlot()
+		slot.configure(at: position)
+		addChild(slot)
+		slots.append(slot)
 	}
 
 }
