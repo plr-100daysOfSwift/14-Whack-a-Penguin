5e0c8c2f9f6633eb4d068276652cebb6dea2c6bd
Paul Richardson <developer@photographerafoot.com>
2021-05-09 10:38:17 +1000

    CHG: Set up background and score

--- Whack-a-Penguin/GameScene.swift
+++ Whack-a-Penguin/GameScene.swift
@@ -9,11 +9,30 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    override func didMove(to view: SKView) {
-    }
-    
-		override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
+
+	var gameScore: SKLabelNode!
+	var score = 0 {
+		didSet {
+			gameScore.text = "Score: \(score)"
+		}
+	}
+
+	override func didMove(to view: SKView) {
+		let background = SKSpriteNode(imageNamed: "whackBackground")
+		background.position = CGPoint(x: 512, y: 384)
+		background.blendMode = .replace
+		background.zPosition = -1
+		addChild(background)
+
+		gameScore = SKLabelNode(fontNamed: "Chalkduster")
+		gameScore.text = "Score: 0"
+		gameScore.position = CGPoint(x: 8, y: 8)
+		gameScore.horizontalAlignmentMode = .left
+		gameScore.fontSize = 48
+		addChild(gameScore)
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+	}
 
 }
