5eb17bbbf672869389049c55a3187e0adf10b744
Paul Richardson <developer@photographerafoot.com>
2021-05-09 12:28:04 +1000

    CHG: Make a grid of slots

--- Whack-a-Penguin/GameScene.swift
+++ Whack-a-Penguin/GameScene.swift
@@ -31,6 +31,11 @@ class GameScene: SKScene {
 		gameScore.horizontalAlignmentMode = .left
 		gameScore.fontSize = 48
 		addChild(gameScore)
+
+		for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+		for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
+		for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+		for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 120)) }
 	}
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
